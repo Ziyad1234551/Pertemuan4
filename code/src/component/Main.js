e89861845718bef74a024298b9d415b4d2a49660
Ziyad1234551 <123793479+Ziyad1234551@users.noreply.github.com>
2023-03-23 11:12:21 +0700

    Initial commit

---
+++ code/src/component/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './Footer'
+import Hello from './Hello'
+import Header from './Header'
+export const Main = () => {
+  return (
+    <div>
+        <Header/>
+        <Hello/>
+        <Footer/>
+
+
+    </div>
+  )
+}
+export default Main;
